888fc6bf3872341a63c12e37fe2f638ed461692a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-03-03 12:05:24 -0800

    wrote middleware to validate password and email when updating a profile

--- data/sleepdata/sleepdata-router.js
+++ data/sleepdata/sleepdata-router.js
@@ -4,6 +4,9 @@ const router = require("express").Router();
 
 const database = require("./sleepdata-model");
 
+const validateCanChangeProfile = require("./validateCanChangeProfile-middleware");
+const validateCanChangeEmail = require("./validateCanChangeEmail-middleware");
+
 // GET: get info from a user by user ID (stored in token)
 router.get("/profile", (req, res) => {
     
@@ -24,89 +27,37 @@ router.get("/profile", (req, res) => {
 })
 
 // PUT: update user profile (email and/password)
-router.put("/profile", (req, res) => {
+router.put("/profile", validateCanChangeProfile, validateCanChangeEmail, (req, res) => {
+
+    let dataToInsert = {};
+
+    if (req.body.newEmail)
+        { dataToInsert.email = req.body.newEmail; }
+
+    if (req.body.newPassword)
+        { dataToInsert.password = bcrypt.hashSync(req.body.newPassword, 14); }   
+
+    if (req.body.newFirstName)
+        { dataToInsert.first_name = req.body.newFirstName; }
     
-    if (!req.body || !req.body.currentPassword)
-        { res.status(400).json({message: "You must enter your old password to make any changes to your profile."}) }
-    else
-        {                  
-            const id = req.decodedToken.id;
+    if (req.body.newLastName)
+        { dataToInsert.last_name = req.body.newLastName; }
 
-            let dataToInsert = {};
-
-            const newEmail = req.body.newEmail;
-            const currentPassword = req.body.currentPassword;
-            const newPassword = req.body.newPassword;
-            const newFirstName = req.body.newFirstName;
-            const newLastName = req.body.newLastName;
-
-            // verify that the current password is correct before making any changes.
-            database.getUserByID(id)
-                .then(existingUser => {
-                    
-                    // password is incorrect; stop updating
-                    if (!bcrypt.compareSync(currentPassword, existingUser.password))
-                        { res.status(403).json({message: "Password incorrect. Could not update profile."}); }
-                    else
-                        {
-                            let errorText = "";
-
-                            // user is trying to update email address: make sure email address isn't being used by someone else
-                            if (newEmail)
-                            {
-                                database.getUserBy({email: req.body.email})
-                                    .then(userWithExistingEmail => {
-                                
-                                        // check if new email is already taken (compare ID of account associated with email with the ID in the token)
-                                        if ((userWithExistingEmail.length > 0) && (userWithExistingEmail.id !== req.decodedToken.id))
-                                            {
-                                                errorText = "However, the requested email address is already in use, so it was not updated."
-
-                                                res.status(401).json({message: "The requested email address is already in use."});
-
-                                                res.end();
-                                            }
-                                        else
-                                            { dataToInsert.email = newEmail; }
-                                    })
-                                    .catch(error => {
-                                        res.status(500).json({message: "Server error in validating credentials.", error})
-                                    })
-                            }
-
-                            // user is trying to update password:
-                            if (newPassword)
-                                { dataToInsert.password = bcrypt.hashSync(newPassword, 14); }   
-
-                            if (newFirstName)
-                                { dataToInsert.first_name = newFirstName; }
-                            
-                            if (newLastName)
-                                { dataToInsert.last_name = newLastName; }
-                            
-                            if (Object.entries(dataToInsert).length > 0)
-                            {
-                                database.updateUser(id, dataToInsert)
-                                    .then(users => {
-                                        res.status(200).json({message: "Profile updated."})
-                                    })
-                                    .catch(error => {
-                                        res.status(500).json({message: "Server error in updating profile.", error})
-                                    })
-                            }
-                            else
-                            {
-                                res.status(200).json({message: "No changes were made to your profile."})
-                            }
-
-                        }
+    if (Object.entries(dataToInsert).length > 0)
+        {
+            database.updateUser(req.decodedToken.id, dataToInsert)
+                .then(users => {
+                    res.status(200).json({message: "Profile updated."})
                 })
-                .catch(({stack, call, message, name}) => {
-
-                    res.status(500).json({message: "Server error in validating credentials.", stack, call, message, name})
+                .catch(error => {
+                    res.status(500).json({message: "Server error in updating profile.", error})
                 })
-
         }
+    else
+        {
+            res.status(200).json({message: "No changes were made to your profile."})
+        }
+
 })
 
 // PUT: update user profile
---
+++ data/sleepdata/validateCanChangeEmail-middleware.js
@@ -0,0 +1,25 @@
+const database = require("./sleepdata-model");
+
+module.exports = (req, res, next) => {
+
+    if (!req.body.newEmail)
+        { next(); }
+    else
+        {
+            database.getUserBy({email: req.body.newEmail})
+                .then(userWithExistingEmail => {
+
+                    // verify that the email address isn't being used by someone else (if in use, should be current account).
+                    // Check by comparing ID of account associated with email with the ID in the token
+                    if (!userWithExistingEmail || userWithExistingEmail.id === req.decodedToken.id)
+                        { next(); }
+
+                    // email address is in use by another user
+                    else
+                        { res.status(400).json({message: "Email address already in use."}) }
+                })
+                .catch(error => {
+                    res.status(500).json({message: "Server error in validating credentials.", error})
+                })
+        }
+}
